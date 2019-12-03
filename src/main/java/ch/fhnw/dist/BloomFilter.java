eff06f37bf76b32afccb448170870e6a1ed4d66b
Damian Zehnder <zehnder@adobe.com>
2019-12-03 16:23:56 +0100

    reading parameters from commandline

--- src/main/java/ch/fhnw/dist/BloomFilter.java
+++ src/main/java/ch/fhnw/dist/BloomFilter.java
@@ -2,12 +2,22 @@ package ch.fhnw.dist;
 
 public class BloomFilter {
 
+    int n;
+    double p;
+
     /**
+     *
      *
      * @param n Amount of expected elements
      * @param p The desired false positive
      */
     public BloomFilter(int n, double p) {
+        this.n = n;
+        this.p = p;
 
+        System.out.println(n + p);
     }
+
+
+
 }
--- src/main/java/ch/fhnw/dist/Start.java
+++ src/main/java/ch/fhnw/dist/Start.java
@@ -1,8 +1,22 @@
 package ch.fhnw.dist;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Start {
 
     public static void main(String[] args) {
 
+        File words = new File("words.txt");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the desired false positive percentage: ");
+        double p = scanner.nextDouble();
+
+        System.out.println("Please enter the amount of expected elements: ");
+        int n = scanner.nextInt();
+
+        BloomFilter bloomFilter = new BloomFilter(n, p);
     }
 }
