dfd955c265a71f2120c5ad419eacb3412c0d99b5
Damian Zehnder <zehnder@adobe.com>
2019-12-03 15:51:59 +0100

    adding inital project strcture

---
+++ src/main/java/ch/fhnw/dist/BoomFilter.java
@@ -0,0 +1,13 @@
+package ch.fhnw.dist;
+
+public class BoomFilter {
+
+    /**
+     *
+     * @param n Amount of expected elements
+     * @param p The desired false positive
+     */
+    public BoomFilter(int n, double p) {
+
+    }
+}
