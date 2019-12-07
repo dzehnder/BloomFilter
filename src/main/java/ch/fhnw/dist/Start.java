42f97544ac7353e56359f0c9d67096d49f1761a4
Mario <mario.wettstein@students.fhnw.ch>
2019-12-07 13:48:32 +0100

    Temp Upload for tests

--- src/main/java/ch/fhnw/dist/Start.java
+++ src/main/java/ch/fhnw/dist/Start.java
@@ -8,24 +8,45 @@ public class Start {
 
     public static void main(String[] args) throws FileNotFoundException {
 
+        //- Selection from File enable/disable
+        boolean selWordControl = true;
+        //- Check from file enable/disable
+        boolean chckWordControl = false;
+        //- --> if both have them same value: read the same Words
+
+        Scanner scanner = new Scanner(System.in);
+
         //- Init FileReader
         File words = new File("words.txt");
         Scanner FileReader = new Scanner(words);
 
-        int numberOfWords = 10;
-        for(int i = 0; i < numberOfWords; i++)
+        BloomFilter bloomFilter = new BloomFilter(n, p);
+
+        //- Add every second Word from File
+        while(FileReader.hasNext())
         {
-            System.out.println(FileReader.next());
+            if(selWordControl)
+            {
+                bloomFilter.add(FileReader.next());
+            }
+
+            selWordControl = (selWordControl==false) ? true : false;
         }
 
-        Scanner scanner = new Scanner(System.in);
+       //- Check every second Word from File
+        while(FileReader.hasNext())
+        {
+            if(chckWordControl)
+            {
+                bloomFilter.hasWord(FileReader.next());
+            }
 
-        System.out.println("Please enter the desired false positive percentage: ");
-        double p = scanner.nextDouble();
+            chckWordControl = (chckWordControl==false) ? true : false;
+        }
 
-        System.out.println("Please enter the amount of expected elements: ");
-        int n = scanner.nextInt();
 
-        BloomFilter bloomFilter = new BloomFilter(n, p);
+        //- Close Open Scanner
+        FileReader.close();
+
     }
 }
