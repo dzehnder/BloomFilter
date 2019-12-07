c0ba58734d1cc8d6b5ddd77c3cd395b59652d805
Mario <mario.wettstein@students.fhnw.ch>
2019-12-07 14:41:41 +0100

    - Fixed n and p - limit while with n

--- src/main/java/ch/fhnw/dist/Start.java
+++ src/main/java/ch/fhnw/dist/Start.java
@@ -13,8 +13,17 @@ public class Start {
         //- Check from file enable/disable
         boolean chckWordControl = false;
         //- --> if both have them same value: read the same Words
+        int i = 0;
+        int j = 0;
 
         Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Please enter the amount of expected elements: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Please enter the desired false positive percentage: ");
+        double p = scanner.nextDouble();
 
         //- Init FileReader
         File words = new File("words.txt");
@@ -23,7 +32,7 @@ public class Start {
         BloomFilter bloomFilter = new BloomFilter(n, p);
 
         //- Add every second Word from File
-        while(FileReader.hasNext())
+        while(FileReader.hasNext() & i != n)
         {
             if(selWordControl)
             {
@@ -31,10 +40,11 @@ public class Start {
             }
 
             selWordControl = (selWordControl==false) ? true : false;
+            i++;
         }
 
        //- Check every second Word from File
-        while(FileReader.hasNext())
+        while(FileReader.hasNext() & j != n)
         {
             if(chckWordControl)
             {
@@ -42,6 +52,7 @@ public class Start {
             }
 
             chckWordControl = (chckWordControl==false) ? true : false;
+            j++;
         }
 
 
