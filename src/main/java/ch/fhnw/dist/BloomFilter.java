0fb92d558a731812528b5e120ade3481c49c14e2
Damian Zehnder <zehnder@adobe.com>
2019-12-07 13:51:28 +0100

    adding guava dependency

--- src/main/java/ch/fhnw/dist/BloomFilter.java
+++ src/main/java/ch/fhnw/dist/BloomFilter.java
@@ -1,9 +1,19 @@
 package ch.fhnw.dist;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.base.Charsets;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+
 public class BloomFilter {
 
     int n, m;
     double p, k;
+    int[] bitsArray;
+    List<HashFunction> hashes = new ArrayList<>();
 
     /**
      *
@@ -15,21 +25,47 @@ public class BloomFilter {
         this.n = n;
         this.p = p;
 
+        // calculate the number of bits in the array, round up with 1 at the end
         m = (int) -((n * Math.log(p)) / Math.pow(Math.log(2), 2)) + 1;
-        k = -(Math.log(p) / Math.log(2));
+
+        // calculate the number of hash functions, round up with 1 at the end
+        k = (int) -(Math.log(p) / Math.log(2)) + 1;
 
         System.out.println("m: " + m);
         System.out.println("k: " + k);
 
-        int[] bitsArray = new int[m];
 
+        bitsArray = new int[m];
+
+        // generate bits array and set every position to 0
         for (int i = 0; i < bitsArray.length; i++) {
             bitsArray[i] = 0;
         }
 
-
+        // generate hashes and add them to the list
+        for (int i = 0; i < k; i++) {
+            hashes.add(Hashing.murmur3_128());
+        }
 
     }
+
+
+    public void add(String word) {
+        for (HashFunction hashFunction : hashes) {
+            HashCode hashCode = hashFunction.newHasher()
+                    .putString(word, Charsets.UTF_8)
+                    .hash();
+        }
+    }
+
+    public boolean hasWord(String word) {
+
+        return false;
+
+    }
+
+
+
 
 
 
